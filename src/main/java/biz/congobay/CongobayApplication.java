01e81402cea754b7e853258371edf868e24d6973
Christopher M. Judd <javajudd@gmail.com>
2015-07-17 16:14:20 -0400

    Added initial creation of project.

---
+++ src/main/java/biz/congobay/CongobayApplication.java
@@ -0,0 +1,12 @@
+package biz.congobay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongobayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CongobayApplication.class, args);
+    }
+}
