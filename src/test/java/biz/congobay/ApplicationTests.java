8e42a5d191a7ed11d309304d2669e8c9810c8606
Christopher M. Judd <javajudd@gmail.com>
2015-07-17 16:59:08 -0400

    Refactored CongobayApplication to just Application since I just don't like the name of the app in the Application class name.

--- src/test/java/biz/congobay/ApplicationTests.java
+++ src/test/java/biz/congobay/ApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = CongobayApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-public class CongobayApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
