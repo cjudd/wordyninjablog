d456cd91d45f3ac5aac1a0f779d190583d935f08
Christopher M. Judd <javajudd@gmail.com>
2015-07-20 11:20:04 -0600

    Removed unnecessary import.

--- src/main/java/ninja/wordy/blog/web/config/WebSecurityConfig.java
+++ src/main/java/ninja/wordy/blog/web/config/WebSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebMvcSecurity
