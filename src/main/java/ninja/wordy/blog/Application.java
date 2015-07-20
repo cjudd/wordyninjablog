2dca3fe2e8066fe62b0dccedcbc3f115884a2038
Christopher M. Judd <javajudd@gmail.com>
2015-07-19 23:17:47 -0400

    Created an executable jar and a deployable war file.

--- src/main/java/ninja/wordy/blog/Application.java
+++ src/main/java/ninja/wordy/blog/Application.java
@@ -2,11 +2,18 @@ package ninja.wordy.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
