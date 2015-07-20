b2c886e120bd16bee5efff3b9170f5204bd44c15
Christopher M. Judd <javajudd@gmail.com>
2015-07-20 08:36:13 -0600

    Increased the size of columns to hold more data.

--- src/main/java/ninja/wordy/blog/model/Post.java
+++ src/main/java/ninja/wordy/blog/model/Post.java
@@ -1,16 +1,17 @@
 package ninja.wordy.blog.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @Column(length = 500)
     private String title;
+
+    @Column(length=5000)
     private String content;
 
     public long getId() {
