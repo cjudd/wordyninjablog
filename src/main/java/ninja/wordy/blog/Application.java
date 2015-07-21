cb22dda0a6636ce02a5990400b0b787f726f3949
Christopher M. Judd <javajudd@gmail.com>
2015-07-21 06:21:04 -0600

    Added code to be able to disable httponly session cookies.

--- src/main/java/ninja/wordy/blog/Application.java
+++ src/main/java/ninja/wordy/blog/Application.java
@@ -1,12 +1,17 @@
 package ninja.wordy.blog;
 
+import org.apache.catalina.Context;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer /* implements EmbeddedServletContainerCustomizer */ {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -16,4 +21,15 @@ public class Application extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
+
+//    @Override
+//    public void customize(final ConfigurableEmbeddedServletContainer container) {
+//        ((TomcatEmbeddedServletContainerFactory) container).addContextCustomizers(new TomcatContextCustomizer() {
+//            @Override
+//            public void customize(Context context) {
+//                context.setUseHttpOnly(false);
+//            }
+//        });
+//    }
+
 }
