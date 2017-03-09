d9f2bc9de9483b3f96199b7d65aae2a4289098a1
Christopher M. Judd <javajudd@gmail.com>
2017-03-09 16:30:15 -0500

    Removing manual sql files after adding fly way for managing database.

--- src/main/sql/setup.sql
+++
@@ -1,5 +0,0 @@
-create database wordyninjablog;
-CREATE USER 'wordyninja'@'localhost' IDENTIFIED BY 'wordyninja';
-GRANT ALL PRIVILEGES ON wordyninjablog.* TO 'wordyninja'@'localhost';
-FLUSH PRIVILEGES;
-
--- src/main/sql/wordyninjablog.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `post` VALUES (1,'But Groovy is Better.','2015-07-21 00:33:46','Java Rocks!!!');
-
-INSERT INTO `role` VALUES (1,'Standard User','ROLE_USER'),(2,'Blogger User','ROLE_BLOGGER'),(3,'Admin User','ROLE_ADMIN');
-
-INSERT INTO `user` VALUES (1,'Super','Administrator','c93ccd78b2076528346216b3b2f701e6','admin'),(2,'Blogger1','Blogger1','fada6fe346baf66f53eaf4e05fedf662','blogger1'),(3,'Blogger2','Blogger2','fada6fe346baf66f53eaf4e05fedf662','blogger2'),(4,'Reader1','Reader1','721ab316eb0233a02ae8f4ccec08d8b8','reader1'),(5,'Reader2','Reader2','721ab316eb0233a02ae8f4ccec08d8b8','reader2');
-
-INSERT INTO `user_role` VALUES (1,1),(2,1),(3,1),(1,2),(2,2),(3,2),(1,3);
